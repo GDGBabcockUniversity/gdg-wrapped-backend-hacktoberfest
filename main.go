2cbbdab8f3ced2f8a4a43f825e9fd1f9a2e0bf1c
Philip Edekobi <philipedekobi@gmail.com>
2023-12-28 16:11:29 +0100

    cors

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -107,6 +109,15 @@ func setupRoutes() *gin.Engine {
 
 func main() {
 	server := setupRoutes()
+
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	server.Run()
 }
